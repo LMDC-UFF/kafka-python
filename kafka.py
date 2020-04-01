1d59b1d37d26503662057ece182d720a9a2c8bd0
Jose Huillca <josehuillca@Joses-MacBook-Pro.local>
2020-04-01 09:51:46 -0300

    check all functions

--- kafka.py
+++ kafka.py
@@ -1,40 +1,40 @@
 import os
 import json
+import sys
 from confluent_kafka import Producer, Consumer, KafkaError
 
 
 # Biblioteca para Python & Kafka
+# Read: https://docs.confluent.io/current/installation/configuration/consumer-configs.html
 
 class kafkaAPI():
 
     @staticmethod
-    def createConsumerWithKerberos(kafka_ip: str, group_id: str, securityProtocol: str, saslMechanism: str,
-                                   saslKerberosServiceName: str, kerberosKeyTabPath: str, kerberosUsername: str):
-
+    def createConsumerWithKerberos(kafka_ip: str, group_id: str, kerberosKeyTabPath: str, kerberosUsername: str,
+                                   autoOffsetReset: str='earliest', securityProtocol: str='SASL_PLAINTEXT'):
         conf = {
             'bootstrap.servers': kafka_ip,
             'group.id': group_id,
-            'auto.offset.reset': 'earliest',
+            'auto.offset.reset': autoOffsetReset,
             'security.protocol': securityProtocol,
-            'sasl.mechanism': saslMechanism,
-            'sasl.kerberos.service.name': saslKerberosServiceName,
+            'sasl.mechanism': 'GSSAPI',
+            'sasl.kerberos.service.name': 'kafka',
             'sasl.kerberos.keytab': kerberosKeyTabPath,
             'sasl.kerberos.principal': kerberosUsername
         }
-
 
         c = Consumer(conf)
         return c
 
     @staticmethod
-    def createConsumerWithLoginAndPass(kafka_ip: str, group_id: str, securityProtocol: str, saslMechanism: str,
-                                       kafkaUserName: str, kafkaPassword: str):
+    def createConsumerWithLoginAndPass(kafka_ip: str, group_id: str, kafkaUserName: str, kafkaPassword: str,
+                                       autoOffsetReset: str='earliest', securityProtocol: str='SASL_PLAINTEXT'):
 
         conf = {'bootstrap.servers': kafka_ip,
                 'group.id': group_id,
-                'auto.offset.reset': 'earliest',
+                'auto.offset.reset': autoOffsetReset,
                 'security.protocol': securityProtocol,
-                'sasl.mechanism': saslMechanism,
+                'sasl.mechanism': 'PLAIN',
                 'sasl.username':kafkaUserName,
                 'sasl.password': kafkaPassword
         }
@@ -43,63 +43,50 @@ class kafkaAPI():
         return c
 
     @staticmethod
-    def createConsumerWithoutLogin(kafka_ip: str, group_id: str):
+    def createConsumerWithoutLogin(kafka_ip: str, group_id: str, autoOffsetReset: str='earliest'):
         conf = {
             'bootstrap.servers': kafka_ip,
             'group.id': group_id,
-            'auto.offset.reset': 'earliest'
+            'auto.offset.reset': autoOffsetReset
         }
 
         c = Consumer(conf)
         return c
 
     @staticmethod
-    def createProducer_kerberos(detections: dict, kafka_ip: str, topic_name: str,
-                                     kafka_sasl_mechanism: str, kafka_krb5_user_keytab_path: str, kafka_krb5_username: str, kafka_debug: bool):
-        sasl_mechanism = kafka_sasl_mechanism
+    def createProducerWithKerberos(kafka_ip: str, kafka_krb5_user_keytab_path: str, kafka_krb5_username: str,
+                                   securityProtocol: str='SASL_PLAINTEXT', kafka_debug: bool=False):
         producer_conf = {'bootstrap.servers': kafka_ip,
-                         'security.protocol': 'SASL_PLAINTEXT',
-                         'sasl.mechanism': sasl_mechanism,
+                         'security.protocol': securityProtocol,
+                         'sasl.mechanism': 'GSSAPI',
                          'sasl.kerberos.service.name': 'kafka',
                          'sasl.kerberos.keytab': kafka_krb5_user_keytab_path,
                          'sasl.kerberos.principal': kafka_krb5_username}
 
-
         try:
-            # 'KAFKA_DEBUG' has to be in os.environ
             if kafka_debug:
                 producer_conf['debug'] = 'security,broker'
-
             producer = Producer(**producer_conf)
-
             return producer
-
         except BufferError:
-            sys.stderr.write(
-                '%% Local producer queue is full (%d messages awaiting delivery): try again\n' % len(producer))
-            # producer.poll(0)
-            # Wait until all messages have been delivered
-            # sys.stderr.write('%% Waiting for %d deliveries\n' % len(p))
-            # p.flush()
+            sys.stderr.write('%% Local producer queue is full (%d messages awaiting delivery): try again\n' % len(producer))
+            return None
+
+    def createProducerWithLoginAndPass(kafka_ip: str, group_id: str, kafkaUserName: str, kafkaPassword: str,
+                                       kafka_debug: bool=False):
+        # check this function
+        pass
 
     @staticmethod
-    def createProducer_without_login(kafka_ip: str, topic_name: str, kafka_sasl_mechanism: str, kafka_debug: bool):
-        sasl_mechanism = kafka_sasl_mechanism
-        producer_conf = {'bootstrap.servers': kafka_ip}
-
+    def createProducerWithoutLogin(kafka_ip: str, topic_name: str, kafka_debug: bool=False):
         try:
-            # 'KAFKA_DEBUG' has to be in os.environ
+            producer_conf = {'bootstrap.servers': kafka_ip}
             if kafka_debug:
                 producer_conf['debug'] = 'security,broker'
 
             producer = Producer(**producer_conf)
-
             return producer
 
         except BufferError:
-            sys.stderr.write(
-                '%% Local producer queue is full (%d messages awaiting delivery): try again\n' % len(producer))
-            # producer.poll(0)
-            # Wait until all messages have been delivered
-            # sys.stderr.write('%% Waiting for %d deliveries\n' % len(p))
-            # p.flush()
+            sys.stderr.write('%% Local producer queue is full (%d messages awaiting delivery): try again\n' % len(producer))
+            return None
